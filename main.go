9c65bdc48af22db3679f58adbce5ce00aa3b6928
liuyuzhe-at-doordash-infra <yuzhe.liu@doordash.com>
2022-09-12 15:43:26 -0700

    create pulumi stack from the imported tf-created s3 bucket my-test-s3bucket-20220912222806820000000001

--- main.go
+++ main.go
@@ -6,25 +6,13 @@ import (
 )
 
 func main() {
-	//pulumi.Run(func(ctx *pulumi.Context) error {
-	//	// Create an AWS resource (S3 Bucket)
-	//	bucket, err := s3.NewBucket(ctx, "my-tmp-test-pulumi-bucket1", nil)
-	//	if err != nil {
-	//		return err
-	//	}
-	//
-	//	// Export the name of the bucket
-	//	ctx.Export("bucketName", bucket.ID())
-	//	return nil
-	//})
 
 	/* generated from cmd:
-	pulumi import aws:s3/bucket:Bucket my-test-pulumi-s3-result-from-import-tf-s3 my-test-s3bucket-20220912203139557000000001
-	*/
+	pulumi import aws:s3/bucket:Bucket my-test-pulumi-s3-result-from-import-tf-2nd-s3 my-test-s3bucket-20220912222806820000000001	*/
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		mybucket, err := s3.NewBucket(ctx, "my-test-pulumi-s3-result-from-import-tf-s3", &s3.BucketArgs{
-			Arn:          pulumi.String("arn:aws:s3:::my-test-s3bucket-20220912203139557000000001"),
-			Bucket:       pulumi.String("my-test-s3bucket-20220912203139557000000001"),
+		_, err := s3.NewBucket(ctx, "my-test-pulumi-s3-result-from-import-tf-2nd-s3", &s3.BucketArgs{
+			Arn:          pulumi.String("arn:aws:s3:::my-test-s3bucket-20220912222806820000000001"),
+			Bucket:       pulumi.String("my-test-s3bucket-20220912222806820000000001"),
 			HostedZoneId: pulumi.String("Z2O1EMRO9K5GLX"),
 			RequestPayer: pulumi.String("BucketOwner"),
 			Tags: pulumi.StringMap{
@@ -35,12 +23,6 @@ func main() {
 				Enabled: pulumi.Bool(true),
 			},
 		}, pulumi.Protect(true))
-
-		_, err = s3.NewBucketObject(ctx, "test-object2.txt", &s3.BucketObjectArgs{
-			Bucket: mybucket.ID(),
-			Source: pulumi.NewFileAsset("test-object2.txt"),
-		})
-
 		if err != nil {
 			return err
 		}
