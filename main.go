8b1dbdafbf1a6e60438cfeccffe12b44aee0031b
liuyuzhe-at-doordash-infra <yuzhe.liu@doordash.com>
2022-09-12 17:32:11 -0700

    update bucket since tf-creat it so need to import

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 func main() {
 
 	/* generated from cmd:
-	pulumi import aws:s3/bucket:Bucket my-test-pulumi-s3-result-from-import-tf-2nd-s3 my-test-s3bucket-20220912222806820000000001	*/
+	pulumi import aws:s3/bucket:Bucket my-test-pulumi-s3-result-from-import-tf-2nd-s3 my-test-s3bucket-my-test-s3bucket-20220913002919871500000001	*/
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		my2ndBucket, err := s3.NewBucket(ctx, "my-test-pulumi-s3-result-from-import-tf-2nd-s3", &s3.BucketArgs{
-			Arn:          pulumi.String("arn:aws:s3:::my-test-s3bucket-20220912222806820000000001"),
-			Bucket:       pulumi.String("my-test-s3bucket-20220912222806820000000001"),
+			Arn:          pulumi.String("arn:aws:s3:::my-test-s3bucket-20220913002919871500000001"),
+			Bucket:       pulumi.String("my-test-s3bucket-20220913002919871500000001"),
 			HostedZoneId: pulumi.String("Z2O1EMRO9K5GLX"),
 			RequestPayer: pulumi.String("BucketOwner"),
 			Tags: pulumi.StringMap{
@@ -22,7 +22,7 @@ func main() {
 			Versioning: &s3.BucketVersioningArgs{
 				Enabled: pulumi.Bool(true),
 			},
-		}, pulumi.Import(pulumi.ID("my-test-s3bucket-20220912222806820000000001")),
+		}, pulumi.Import(pulumi.ID("my-test-s3bucket-20220913002919871500000001")),
 		)
 		//pulumi.Protect(true))
 		if err != nil {
